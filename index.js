9ee4f65c2c804fd390bdc90d767fc9683f0af427
Arkessiah <amarotoenriquez@gmail.com>
2021-11-25 16:05:12 +0000

    finish

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Declara dos variables llamadas 'nombre' y 'apellido' con los valores "Juan" y "Upgrader" respectivamente.
-
+var nombre = "Juan";
+var apellido = "Upgrader";
 
 // Declara una variable llamada 'nombreCompleto' que sea igual a la suma de nombre y apellido. ¡Sepáralas con un espacio!
-
+var nombreCompleto = nombre + " " + apellido;
 
 // Declara ahora una variable llamada 'edad' que valga 28 (número).
-
+var edad = 28;
 
 // Declara finalmente una variable llamada 'mensaje' que diga "Mi nombre es NOMBRE COMPLETO y tengo X años" donde NOMBRE COMPLETO será la variable nombreCompleto y X será la variable edad. 
-
+var mensaje = "Mi nombre es " + nombreCompleto + " y tengo " + edad +  " años";
